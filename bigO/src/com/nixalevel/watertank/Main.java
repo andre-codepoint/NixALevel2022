d127350b1d067175d402a4bb8c0347ffc3647f9b
userone nixedu <useronenixedu@gmail.com>
2022-08-25 12:18:42 +0300

    v.2.3

---
+++ bigO/src/com/nixalevel/watertank/Main.java
@@ -0,0 +1,8 @@
+package com.nixalevel.watertank;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Solution.maxArea(new int[]{1,1}));
+        System.out.println(Solution.maxArea(new int[]{1,8,6,2,5,4,8,3,7}));
+    }
+}
